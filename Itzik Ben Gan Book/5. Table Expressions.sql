e9d8351931e3b8545398678e8986c1251b7d7319
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-02-25 14:16:10 +0100

    finished reading table expressions

--- Itzik Ben Gan Book/5. Table Expressions.sql
+++ Itzik Ben Gan Book/5. Table Expressions.sql
@@ -276,6 +276,41 @@ To avoid confusion, the best practice is to explicitly list the column names you
 definition of the view. If columns are added to the underlying tables and you need them in the
 view, use the ALTER VIEW statement to revise the view definition accordingly.
 
+	ENCRYPTION option
+
+Encryption option is available when we create/ alter views, triggers, stored procedures,
+and user-defined functions. This tells the SQL Server to store text with the definition of
+the object in an obfuscated format.
+
+The obfuscated text is not directly visible to users through any of the catalog objects --
+only to privileged users through special means
+
+NOTE: When we alter a view, we would not be able to retain the exsisting permissions if not 
+explicitly specified. So if we have an ENCRYPTION option on the CREATE VIEW statement, when
+we alter the existing view, we should re-specify the ENCRYPTION option, otherwise, it would be 
+lost.
+
+	SCHEMABINDING option
+
+ This option is available to view and UDFs and it helps to bind the schema/ columns of the 
+ referenced objects to that of the referencing objects. What this does is, it makes it impossible
+ to drop columns and table of the referenced objects.
+
+ NOTE: Please note that the SCHEMABINDING option can only be used for view/ UDFs definition
+ wthout the "*" in the SELECT statement. Hence, columns should be explicitly specified within
+ the SELECT clause. Also, 2-part schema-qualified names should be used when referring to the 
+ objects. 
+ 
+	CHECK OPTION
+
+THis helps to prevent the modification of a view except it satisfies the condition of the 
+view definition. For example, if a view filters out customers that are non-USA, with a 
+CHECK OPTION enabled, it will not be possible to update or mlodify the view with a rows of 
+information of customers that are non-USA. Otherwise, without CHECK option the update
+operation will go through the view and update the relation of the underlying object.
+	 
+
+
 */
 
 -- Let us create a view called USA_Custs
@@ -342,3 +377,237 @@ GO
 -- be guaranteed with an ORDER BY clause.
 SELECT *
 FROM Sales.USACusts;
+
+-- To use the ENCRYPTION option. Let us first alter the view of USACusts
+
+ALTER VIEW Sales.USACusts
+AS
+
+SELECT 
+	*
+FROM Sales.Customers
+WHERE country = 'USA';
+GO
+
+-- To get the definition of the view, we invoke the OBJECT_DEFINITION.
+
+SELECT OBJECT_DEFINITION(OBJECT_ID('Sales.USACusts'));
+
+-- We were able to see the text with the view definition. To make this unavailable, we use
+-- the ENCRYPTION option together with the VIEW definition.
+
+ALTER VIEW Sales.USACusts WITH ENCRYPTION
+AS
+
+SELECT 
+	*
+FROM Sales.Customers
+WHERE country = 'USA';
+GO
+
+-- Lets try to view the object definition again. we will try use sp_helptext
+
+SELECT OBJECT_DEFINITION(OBJECT_ID('Sales.USACusts'));
+
+EXEC sp_helptext 'Sales.USACusts';
+
+EXEC sp_helptext 'HR.Employees';
+
+-- Lets try the use of schema-binding
+
+ALTER VIEW Sales.USACusts WITH SCHEMABINDING
+AS
+SELECT
+	*
+FROM Sales.Customers
+WHERE country = N'USA';
+GO
+
+-- The attempt to alter above failed because the '*' is used. Hence, columns should be
+-- properly named and specified
+
+ALTER VIEW Sales.USACusts WITH SCHEMABINDING
+AS
+
+SELECT 
+	custid, companyname, contactname, contacttitle, address,
+	city, region, postalcode, country, phone, fax
+FROM Sales.Customers
+WHERE country = 'USA';
+GO
+
+-- Let us now try to drop the address column from the customers table, we therefore
+-- get an error that says we are now allowed to drop the column.  
+
+ALTER TABLE Sales.Customers DROP COLUMN address;
+
+SELECT *
+FROM Sales.Customers;
+
+/* Using CHECK OPTION*/
+
+-- Let us try to insert a row of info of a customer to from the UK into the USACusts view
+
+INSERT INTO Sales.USACusts(
+	companyname, contactname, contacttitle, address, city,
+	region, postalcode, country, phone, fax)
+VALUES(
+	N'Customer ABCDE', N'Contact ABCDE', N'Title ABCDE', N'Address ABCDE',
+	N'London', NULL, N'12345', N'UK', N'012-3456789', N'012-3456789');
+
+-- If we check the row above in USACusts view, it returns an empty set.
+
+/*
+DELETE FROM Sales.Customers
+WHERE custid > 91;
+*/
+
+SELECT custid, companyname, country
+FROM Sales.USACusts
+WHERE contactname = 'Contact ABCDE';
+
+-- However, when we check for the info in the sales.Customers table, we find the info
+-- This is because there is no WITH CHECK OPTION to prevent the update of info that did not
+-- satisfy the view definition.
+
+SELECT custid, companyname, country
+FROM Sales.Customers
+WHERE contactname = 'Contact ABCDE';
+
+-- To prevent the addition of info that did not satisfy the view definition, we use
+-- WITH CHECK OPTION
+
+ALTER VIEW Sales.USACusts WITH SCHEMABINDING
+AS
+
+SELECT
+	companyname, contactname, contacttitle, address,
+	city, region, postalcode, country, phone, fax
+FROM Sales.Customers
+WHERE country = 'USA'
+WITH CHECK OPTION;
+GO
+
+-- Lets try insert another customer from the UK. This result in an error as the row
+-- did not satisfy the view definition
+
+INSERT INTO Sales.USACusts(
+	companyname, contactname, contacttitle, address,
+	city, region, postalcode, country, phone, fax)
+VALUES(
+	N'Customer FGHIJ', N'Contact FGHIJ', N'Title FGHIJ', N'Address FGHIJ',
+	N'London', NULL, N'12345', N'UK', N'012-3456789', N'012-3456789');
+
+-- Sweet yeah! We are unable to add rows of info that did not satisfy the view definition
+
+DELETE FROM Sales.Customers
+WHERE custid > 91;
+
+IF OBJECT_ID(N'Sales.USACusts', N'U') IS NOT NULL DROP VIEW Sales.USACusts;
+
+
+/*
+	INLINE TABLE-VALUED FUNCTIONS
+
+Inline TVFs are reusable table expressions that support input parameters. In most respects,
+except for the support of input parameters, Inline TVFs are similar to views. IN this respect,
+inline TVFs are referred to as parmeterized views, even though they are not formally referred
+to this way.
+ 
+T-SQL supports another type of table function called multi-statement TVF, which populates
+and returns a table variable. This type is not considered a table expression because 
+it is not based on a query.  
+
+*/
+
+-- The below code creates an inline TVFs called GetCustOrders
+-- Inline TVFs accept input parameters and are specified inside a parentheses following a 
+-- function's name
+
+USE TSQLV4;
+IF OBJECT_ID('dbo.GetCustOrders', 'U') IS NOT NULL DROP FUNCTION dbo.GetCustOrders;
+GO
+CREATE FUNCTION dbo.GetCustOrders
+	(@cid AS INT) RETURNS TABLE
+AS
+RETURN 
+	SELECT orderid, custid, empid, orderdate, requireddate,
+	shippeddate, shipperid, freight, shipname, shipaddress, shipcity,
+	shipregion, shippostalcode, shipcountry 
+	FROM Sales.Orders
+	WHERE custid = @cid;
+GO
+
+-- LEts query the inline TVFs. This returns orders with customer id of 1
+
+SELECT orderid, custid
+FROM dbo.GetCustOrders(1) AS O;
+
+-- As with tables, inline TVFs can be used in JOIN operations
+
+SELECT O.orderid, O.custid, OD.productid, OD.qty
+FROM dbo.GetCustOrders(1) AS O INNER JOIN Sales.OrderDetails AS OD
+	ON O.orderid = OD.orderid;
+
+-- Lets cleanup
+
+IF OBJECT_ID('dbo.GetCustOrders', 'U') IS NOT NULL DROP FUNCTION dbo.GetCustOrders;
+
+/*
+	APPLY operator
+
+This is used in the FROM clause of a query. There are 2 supported types of APPLY: 
+CROSS APPLY and OUTER APPLY. APPLY performs its job in the logical-query processing phase.
+CROSS APPLY implements onely ONE(1) logical-query processing phase whereas OUTER APPLY 
+implements TWO(2).
+
+NOTE: APPLY is not standard. The standard counterpart is called LATERAL, but the standard 
+form was not implemented in SQL server.
+
+APPLY operator operates on two input tables; I'll refer to them as "left" and "right" tables.
+The right table is typically a derived table or a TVF. CROSS APPLY work more like a CROSS JOIN.
+However, CROSS JOIN treats 2 input tables as a set, meaning, one cxanot refer one element on
+one side to elements from the other. with APPLY, the left side is evaluated first. Then the right
+right is then evaluated per row from the left.
+*/
+
+-- Let us use CROSS APPLY to return the 3 most recent orders by customer. We can think of
+-- A as a correlated derived table.
+
+SELECT C.custid, A.orderid, A.orderdate
+FROM Sales.Customers AS C
+	CROSS APPLY
+	(SELECT TOP (3)
+		orderid, empid, orderdate, requireddate
+	 FROM Sales.Orders AS O
+	 WHERE C.custid = O.custid
+	 ORDER BY orderdate DESC, orderid DESC) AS A;
+
+-- In the query above, if the right table returns an empty set, CROSS APPLY will not return 
+-- elements from the left table. To fetch out these result(customers with no top 3 recent orders),
+-- OUTER APPLY is used similar to how LEFT JOIN works.
+
+
+-- We might find it convenient to work with inline TVFs instead of derived tables.
+-- The below code creates an inline TVF called TopOrders that accepts as inputs a customer
+-- ID(@custid) and a number (@n), and returns the @n most recent orders for customer @custid
+
+IF OBJECT_ID('dbo.TopOrders') IS NOT NULL DROP FUNCTION dbo.TopOrders;
+GO
+CREATE FUNCTION dbo.TopOrders
+	(@custid AS INT, @n AS INT)
+	RETURNS TABLE
+AS
+RETURN 
+	SELECT TOP (@n) orderid, empid, orderdate, requireddate
+	FROM Sales.Orders
+	WHERE custid = @custid
+	ORDER BY orderdate DESC, orderid DESC;
+GO
+
+-- USing the correlated inline TVFs
+
+SELECT C.custid, T.orderid, T.orderdate
+FROM Sales.Customers AS C
+	CROSS APPLY
+		dbo.TopOrders(C.custid, 3) AS T;
