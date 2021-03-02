2daa366a3d25e202b2a59dc26cb5a87d687190f2
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-03-02 13:58:36 +0100

    finished chapter 7 on window functions

--- Itzik Ben Gan Book/7. Beyond the fundamentals of querying.sql
+++ Itzik Ben Gan Book/7. Beyond the fundamentals of querying.sql
@@ -198,6 +198,24 @@ This section teaches you how to handle pivoting with T-SQL for cases you do deci
 pivoting with T-SQL for cases you do decide to handle in the database.
 
 Pivoting involves 3 logical processing phase: grouping, spreading and aggregating
+
+	PIVOTING with the PIVOT OPERATOR
+
+the PIVOT operator takes a table as left input, pivots the data, and returns a result table.
+
+Syntax: 
+
+SELECT ...
+FROM <input_table>
+	PIVOT(<agg_function>(<aggregation_element>)
+			FOR <spreading_element> IN (<list_of_target_columns>)) AS <result_table_alias>
+WHERE .....;
+
+NOTE: With the PIVOT operator, we do not specify the grouping elements, it is usually implied.
+Hence, after specifying the spreading elements and the aggregating elements. THe remaining
+attributes are used as grouping elements. Therefore, it is important to only use attributes
+necessary in the table expression.
+
 */
 
 USE TSQLV4;
@@ -238,3 +256,192 @@ FROM dbo.Orders
 GROUP BY empid, custid;
 
 -- Pivoting using explicit groupby, spread and agg operator
+
+SELECT empid,
+	SUM(CASE WHEN custid = 'A' THEN qty END) AS A,
+	SUM(CASE WHEN custid = 'B' THEN qty END) AS B,
+	SUM(CASE WHEN custid = 'C' THEN qty END) AS C,
+	SUM(CASE WHEN custid = 'D' THEN qty END) AS D
+FROM dbo.Orders
+GROUP BY empid
+
+-- Pivoting with PIVOT OPERATOR
+
+SELECT empid, A, B, C, D
+FROM (SELECT empid, custid, qty
+		FROM dbo.Orders) AS D
+		PIVOT(SUM(qty) FOR custid IN (A, B, C,D)) AS T;
+
+-- Using PIVOT operator when spreading elements contain digits, we use delimiters like 
+-- Square brackets
+
+SELECT custid, [1], [2], [3]
+FROM (SELECT empid, custid, qty
+		FROM dbo.Orders) AS D
+	PIVOT(SUM(qty) FOR empid IN ([1], [2], [3])) AS T;
+
+
+/*
+	UNPIVOT DATA
+
+Unpivoting can be done using the APPLY operator or using the UNPIVOT operator.
+
+APPLY operator makes use of the 3 approaches: generating copies, extracting elements and 
+eliminating NULL intersections.
+
+Syntax:
+
+UNPIVOT operator
+
+SELECT ...
+FROM <input_table>
+	UNPIVOT(<values_column> FOR <names_column> IN(<source_columns>)) AS <result_table_alias>
+WHERE ....;
+*/
+USE TSQLV4;
+
+IF OBJECT_ID('dbo.EmpCustOrders', 'U') IS NOT NULL DROP TABLE dbo.EmpCustOrders;
+-- DROP TABLE IF EXISTS dbo.EmpCustOrders;
+
+CREATE TABLE dbo.EmpCustOrders
+(
+	empid INT NOT NULL
+		CONSTRAINT PK_EmpCustOrders PRIMARY KEY,
+	A VARCHAR(5) NULL,
+	B VARCHAR(5) NULL,
+	C VARCHAR(5) NULL,
+	D VARCHAR(5) NULL
+);
+INSERT INTO dbo.EmpCustOrders(empid, A, B, C, D)
+	SELECT empid, A, B, C, D
+	FROM (SELECT empid, custid, qty
+			FROM dbo.Orders) AS D
+		PIVOT(SUM(qty) FOR custid IN(A, B, C, D)) AS P;
+
+
+SELECT * FROM dbo.EmpCustOrders;
+
+-- UNpivot dbo.EmpCustOrders using the CROSS APPLY method
+
+SELECT empid, custid, qty
+FROM dbo.EmpCustOrders
+	CROSS APPLY (VALUES('A', A), ('B', B), ('C', C), ('D', D)) AS C(custid, qty)
+WHERE qty IS NOT NULL;
+
+-- UNpivot dbo.EmpCustOrders using the UNPIVOT method
+
+SELECT empid, custid, qty
+FROM dbo.EmpCustOrders
+	UNPIVOT(qty FOR custid IN(A, B, C, D)) AS T;
+
+-- Let us cleanup
+
+IF OBJECT_ID('dbo.EmpCustOrders', 'U') IS NOT NULL DROP TABLE dbo.EmpCustOrders;
+
+
+/*
+	GROUPING SETS, CUBE and ROLLUP
+	
+These can be used to define multiple grouping sets in GROUP BY clause. They are mainly used
+for reporting and data analysis. There are also the GROUPING and GROUPING_ID functions.
+
+These features usually need the presentation layer to use more sophisticated GUI controls
+to display the data than the typical grid control with its columns and rows.
+
+	GROUPING SETS
+	
+This is a powerful enhancement to the GROUP BY clause. You can use it to define multiple
+grouping sets in the same query. All we need to do is simply list out the grouping sets we
+want in parentheses of the GROUPING SETS subclause and for each grouping set, list the members
+in parentheses followed by commas.
+
+	CUBE Subclause
+
+This is also applicable in a GROUP BY clause. It is an abbreviation for defining multiple
+grouping sets. This helps to group data by all possible subsets of elements in the set passed
+onto it. For example CUBE(a, b, c) is equivalent to GROUPING SETS( (a, b, c), (a,
+b), (a, c), (b, c), (a), (b), (c), () ).
+
+	ROLLUP Subclause
+
+THis provides an abbreviation to the CUBE subclause. Allowing for just a subset of the 
+subset of elements passed into it.
+
+ROLLUP(a, b, c) produces only 4 grouping sets based on the hierarchy a>b>c. 
+Therefore, ROLLUP(a, b, c) ==> GROUPING SETS((a,b,c), (a,b), (a), ())
+
+Also, 
+GROUPING SETS(
+(YEAR(orderdate), MONTH(orderdate), DAY(orderdate)),
+(YEAR(orderdate), MONTH(orderdate)),
+(YEAR(orderdate)),
+() )
+
+is the same as 
+
+ROLLUP(YEAR(orderdate), MONTH(orderdate), DAY(orderdate))
+
+NOTE: When using grouping functions for grouping muktiple sets in the GROUP BY clause, we 
+tend to experience NULLS in the data. This could be as a result of 2 situations. One, it can
+either be that the NULL is part of the data as an aggregate element or it can be that the 
+NULL is used to show that the element is non-participating in the grouping set.
+
+To clear up air on this, the GROUPING function can be used to differentiate between these 
+2 situations. When a column name is passed to the GROUPING function, it returns 1 when
+NULL represents not part of the grouping set(aggregate element) and 
+0 when part of the grouping set. THIS IS COUNTERINTUITIVE. But it is what it is.
+
+GROUPING_ID function is used to get an integer bitmap of the columns passed into it
+
+*/
+
+-- Let us group the Orders table by (empid, custid), (empid), (custid), and ():
+-- The empty () represents the grand total.
+
+SELECT empid, custid, SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY 
+	GROUPING SETS
+	(
+		(empid, custid),
+		(empid),
+		(custid),
+		()
+	);
+
+
+-- The above can also be implemented using CUBE function
+
+SELECT empid, custid, SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY CUBE(empid, custid);
+
+-- Using rollup clause to return a query
+
+SELECT 
+	YEAR(orderdate) AS orderyear,
+	MONTH(orderdate) AS ordermonth,
+	DAY(orderdate) AS orderday,
+	SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY ROLLUP(YEAR(orderdate), MONTH(orderdate), DAY(orderdate))
+
+-- Using GROUPING function to know if NULL is an aggregate element or deatil element(part of the grouping set)
+
+SELECT 
+	GROUPING(empid) AS grpemp,
+	GROUPING(custid) AS grpcust,
+	empid, custid, SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY CUBE(empid, custid);
+
+-- Using GROUPING_ID function. This results in an integer than can be converted to binary form
+-- For (empid, custid): integer 1(binary 01) represents empid is a grouping set and custid is
+-- not a grouping set; integer 2(binary 10) represents empid is not a grouping set and custid
+-- is a grouping set; integer 3(bainary 11) represents empid and custid are not grouping sets
+
+SELECT
+	GROUPING_ID(empid, custid) AS groupingset,
+	empid, custid, SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY CUBE(empid, custid);
